44dc6fac3a4e7f1bdd98d3e6d29d709d35f2c796
Horacio Ansaldi <hnansaldi@gmail.com>
2022-06-25 12:47:08 -0300

    modelado de tabla en php

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
 <?php include 'template/header.php' ?>
 
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <div class="card">
+                <div class="card-header">
+                    Clientes
+                </div>
+                <div class="p-4">
+                    <table class="table align-middle">
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>Nombre Completo</th>
+                                <th>Fecha ingreso equipo</th>
+                                <th>Descripcion del problema</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row"></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td scope="row"></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-4">
+            DOS
+
+        </div>
+    </div>
+</div>
+
 <?php include 'template/footer.php' ?>
